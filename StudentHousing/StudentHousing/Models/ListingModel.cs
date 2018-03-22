745b02cfc682346fb85b8727e03899a907e6425a
clauber_rox <cklauber@gmail.com>
2018-03-21 18:11:52 -0600

    Listing Model Updated No changes on Views

--- StudentHousing/StudentHousing/Models/ListingModel.cs
+++ StudentHousing/StudentHousing/Models/ListingModel.cs
@@ -9,65 +9,54 @@ namespace StudentHousing.Models
     public class ListingModel
     {
         public int Id { get; set; }
-
-        ////Contact Information
-        //public string ContactName { get; set; }
-
-        //public string ContactEmail { get; set; }
-
-        //public string PhoneNumber { get; set; }
-
-        //House General and Location
         [Required]
         public string Name { get; set; }
         [Required]
         public string Description { get; set; }
-        public List<Images> Images { get; set; }
-        //public byte[] Image { get; set; }
-        //public string Address1 { get; set; }
-        //public string Address2 { get; set; }
-        //public string Address3 { get; set; }
-        //public string State { get; set; }
-        //public string City { get; set; }
-        //public int Zip { get; set; }
+        ////Contact Information
+        public string ContactName { get; set; }
 
-        ////House Floorplan
-        //public decimal Bedroom { get; set; }
-        //public decimal Bathroom { get; set; }
-        //public int Kitchen { get; set; }
-        //public decimal SqrFeet { get; set; }
+        public string ContactEmail { get; set; }
 
+        public string PhoneNumber { get; set; }
         ////House Financial Information
-        //public decimal Rent { get; set; }
-        //public bool RentIncludeUtil { get; set; }
-        //public decimal Utilities { get; set; }
-        //public decimal DownPayment { get; set; }
-        //public string Amendities { get; set; }
-
+        public decimal Rent { get; set; }
+        public decimal Utilities { get; set; }
+        public bool RentIncludeUtil { get; set; }
+        public decimal DownPayment { get; set; }
         ////TODO ENUM for contractPeriod
+        //House General and Location
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        ////House Floorplan
+        public int Bedroom { get; set; }
+        public decimal Bathroom { get; set; }
+        public int Kitchen { get; set; }
+        public string Amendities { get; set; }
+        public bool PetFriendly { get; set; }
+        public int ParkingLots { get; set; }
+        public int PeopleNeeded { get; set; }
+        public int RoomsAvailable { get; set; }
+        public bool ManRoom { get; set; }
+        public List<Images> Images { get; set; }
+
+        //TODO: public decimal SqrFeet { get; set; }
 
 
 
 
-        ////Extra Information
-
-        //public bool PetFriendly { get; set; }
 
 
-
-        //public string Utilities { get; set; }
-        //public string Amendities { get; set; }
-
-
-
-
-
-
-
-
-        //public int PeopleSignedUp { get; set; }
-        //public int RoomAvailable { get; set; }
-        //public string MyProperty { get; set; }
+        //////Extra Information
+        ////public bool PetFriendly { get; set; }
+        ////public string Utilities { get; set; }
+        ////public string Amendities { get; set; }
+        ////public int PeopleSignedUp { get; set; }
+        ////public int RoomAvailable { get; set; }
+        ////public string MyProperty { get; set; }
         public List<String> GetImagesToSrc()
         {
             List<String> Imgsources = new List<string>();
